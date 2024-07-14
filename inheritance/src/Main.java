b33853189eb0b3cc923ad9413a0073bce67ef129
Jacob Stephens <jacob.stephens.701@gmail.com>
2024-07-14 18:48:53 -0400

    add inheritance challenge files

---
+++ inheritance/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cylinder can = new Cylinder(5, 10);
+        System.out.printf("The can is %f units", can.getVolume());
+
+    }
+}
