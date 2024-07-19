faa08e3082de30642f0d5c54cd114b2d54be467d
Jacob Stephens <jacob.stephens.701@gmail.com>
2024-07-19 16:44:32 -0400

    use Item array for multiple extras on Burger class rather than multiple Item fields

--- code/src/net/jacobstephens/oop/object_oriented_programming/Burger.java
+++ code/src/net/jacobstephens/oop/object_oriented_programming/Burger.java
@@ -1,9 +1,7 @@
 package net.jacobstephens.oop.object_oriented_programming;
 
 public class Burger extends Item {
-    private Item extra1;
-    private Item extra2;
-    private Item extra3;
+    private Item[] extras;
     
     public Burger(String type, double price) {
 
@@ -21,13 +19,14 @@ public class Burger extends Item {
     }
 
     public void addToppings() {
-        extra1 = new Item("blue cheese", "topping", 1);
-        extra2 = new Item("bacon", "topping", 1.50);
-        extra3 = new Item("caramelized onions", "topping", 0.50);
+        extras = new Item[3];
+        extras[0] = new Item("blue cheese", "topping", 1);
+        extras[1] = new Item("bacon", "topping", 1.50);
+        extras[2] = new Item("caramelized onions", "topping", 0.50);
         name = "Blue Cheese Bacon Burger";
         size = "100g";
-        price += extra1.getBasePrice();
-        price += extra2.getBasePrice();
-        price += extra3.getBasePrice();
+        for (Item extra : extras) {
+            price += extra.getBasePrice();
+        }
     }
 }
